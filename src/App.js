8fecb51382c073e87c8f9d63127c51faf32c3a6f
MortGoldman102 <danielgoldman102@gmail.com>
2020-11-03 14:36:34 +0000

    basic navagtion struction impelemnted

--- src/App.js
+++ src/App.js
@@ -1,47 +1,30 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Home from "./components/Home";
-import Exercise from "./components/exercise"
-import exerciseCategory from "./components/exerciseCategory";
-import About from "./components/About";
+import Navbar from "./Components/Navbar";
+
+
 
 export default function App() {
   return (
     <Router> {/*parent*/}
       <div>{/*child*/}
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <link to="/exercise">Exercise</link>
-            </li>
-            <li>
-              <Link to="/exerciseCategory">ExerciseCategory</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </nav>
+        <Navbar />
+        
 
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/">
-            <Home />
-          </Route>
-          <Route path="/exercise">
-            <Exercise />
-          </Route>
-          <Route path="/exerciseCategory">
-            <ExerciseCategory />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
+         
         </Switch>
+
+
+
+
+
+
+
+
+        
       </div> {/*child*/}
     </Router>//parent 
   );
---
+++ src/Components/Navbar.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+function Navbar(){
+return (
+    <nav>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <link to="/exercise">Exercise</link>
+      </li>
+      <li>
+        <Link to="/BodyPart">ExerciseCategory</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+    </ul>
+  </nav> 
+
+
+)
+}
+export default Navbar;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
