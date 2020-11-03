3c72829acff8f314f0c4d5612fe12f22a5b3e3f1
esthergutierrezr <your-email-address>
2020-11-03 12:42:09 +0000

    npm

--- src/Components/Start.js
+++ src/Components/Start.js
@@ -1,4 +1,12 @@
-import "Start.css";
+import "./Start.css";
 import React from "react";
 
+function Start (){
+    returns (
+        <div>
+            <p>Hello World</p>
+        </div>
+    )
+}
 
+export default Start;
