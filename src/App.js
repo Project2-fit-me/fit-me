e0f46180633dcb083051420f0a336d91df90851b
MortGoldman102 <danielgoldman102@gmail.com>
2020-11-04 16:39:13 +0000

    footer.js and footer.css is all added and the link to the about has been added

--- src/App.js
+++ src/App.js
@@ -1,31 +1,38 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Navbar from "./Components/Navbar";
-
-
+import Footer from "./Components/Footer";
+import About from "./Components/About/About";
 
 export default function App() {
   return (
+    <div className="page-container">
+    <div className="content-wrap">
+
     <Router> {/*parent*/}
-      <div>{/*child*/}
-        <Navbar />
-        
+      <Navbar />    
+      
 
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-         
+
+       
+
+         <Route path="/about">
+           <About />
+         </Route>
+
         </Switch>
+  
+          
 
-
-
-
-
-
-
-
-        
-      </div> {/*child*/}
-    </Router>//parent 
+    
+      
+    </Router>
+    </div>
+    <Footer />
+    </div>
+    
   );
 }
