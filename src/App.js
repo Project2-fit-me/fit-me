e5362365b1c2300557ff51c24d239f43e827dd93
MortGoldman102 <danielgoldman102@gmail.com>
2020-11-05 17:22:38 +0000

    paths and routes been moved to main chaning the component tree

--- src/App.js
+++ src/App.js
@@ -1,33 +1,25 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import {Switch, Route, Redirect} from "react-router-dom";
 import Start from "./Components/Start";
-import Navbar from "./Components/Navbar";
-import Footer from "./Components/Footer";
-import About from "./Components/About/About";
-import BodyPart from "./Components/BodyPart";
+import Main from "./Components/Main";
 
-export default function App(props) {
-  console.log(props);
-  return (
-    <div className="page-container">
-      <div className="content-wrap">
-        {/*parent*/}
-        {location.pathname != "/start" && <Navbar />}
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
+export default class App extends React.Component{
+  
+  render(){
+    return (
+      
         <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/start">
+          <Route exact path="/">
             <Start />
           </Route>
-          <Route path="/body-part">
-            <BodyPart />
+          <Route path="/main">
+           <Main />
           </Route>
-        </Switch>
-        {location.pathname != "/start" && <Footer />}
-      </div>
-    </div>
-  );
+          {/* <Redirect to="/start">
+         </Redirect> */}
+          
+         </Switch>
+       
+    )
+  }
 }
---
+++ src/Components/BodyPart.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+function BodyPart() {
+    return (
+        <div>
+           
+            <h1>Hello</h1>
+        </div>
+    )
+}
+
+export default BodyPart
