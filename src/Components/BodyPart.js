43098dfa51b1d2449d57393a60fddf4e18dd41fd
esthergutierrezr <your-email-address>
2020-11-05 14:31:34 +0000

    about html completed

---
+++ src/Components/BodyPart.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function BodyPart() {
+  return (
+    <div>
+      <p>Helloooooo</p>
+    </div>
+  );
+}
+
+export default BodyPart;
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,22 +1,25 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import './Footer.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Footer.css";
 
 export const Footer = () => {
-    return (
-        <div className="Main-footer">
-            <div className="container">
-                <ul className="Link">
-                    <li>
-                     <Link to="/about" className="underline">Contact Us</Link>
-                    </li>
-                </ul>
-                <p className="col-sm">
-                    &copy;{new Date().getFullYear()} Fit-Me | All right reserved | Terms Of Service
-                </p>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="Main-footer">
+      <div className="container">
+        <ul className="Link">
+          <li>
+            <Link to="/about" className="underline">
+              Contact Us
+            </Link>
+          </li>
+        </ul>
+        <p className="col-sm">
+          &copy;{new Date().getFullYear()} Fit-Me | All right reserved | Terms
+          Of Service
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
