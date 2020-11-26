91a2cc7e00067a5b7468b9eb9af66f8ba057cd85
esthergutierrezr <your-email-address>
2020-11-26 15:14:44 +0000

    favourites button working

--- src/components/exercises-children/ExercisesCard.js
+++ src/components/exercises-children/ExercisesCard.js
@@ -5,20 +5,32 @@ import "./ExercisesCard.css";
 class ExercisesCard extends Component {
   state = {
     isFavorite: false,
+    isInWishList: [],
   };
 
   handleClickFavorite = () => {
     this.setState({ isFavorite: !this.state.isFavorite });
   };
+
+  sendToWishList = () => {
+    // this.setState({ isInWishList: item });
+  };
+
   render() {
     return (
       <div className="card">
-        <p className="name">{this.props.name}</p>
+        <div onClick={() => this.props.displayPopUpCard(this.props.item)}>
+          <p className="name">{this.props.name}</p>
 
-        <img height="200vw" width="300vw" src={this.props.images[0].image} />
+          <img height="200vw" width="300vw" src={this.props.images[0].image} />
+        </div>
         <div
           id="favorite"
-          onClick={this.handleClickFavorite}
+          onClick={
+            this.state.isFavourite
+              ? this.sendToWishList
+              : this.handleClickFavorite
+          }
           className={this.state.isFavorite ? "isFavorite" : "notFavorite"}
         ></div>
       </div>
